18df3379c0de77302fcccb8b751a34f18294803f
Haylee <17357481+hailstormCodes@users.noreply.github.com>
2021-05-12 22:42:18 -0400

    creted data for christmas app

---
+++ src/redux/villages/scenes.data.js
@@ -0,0 +1,30 @@
+const SCENES_DATA =  {
+  pinehilltreefarm: {
+    id: 1,
+    village: 'Holidays & Seasons',
+    category: 'Table Accents',
+    title: "Pine Hill Tree Farm",
+    itemUrl: 'https://www.lemaxcollection.com/retired/holidays-seasons/table-accents/pine-hill-tree-farm-64062',
+    imageUrl: '../../assets/images/houses/01-pinehilltreefarm.png',
+    retired: true,
+    sku: 64073,
+    yearReleased: 2016,
+    yearRetired: 2019,
+    routeName: 'pinehilltreefarm',
+  },
+  swinginggate: {
+    id: 2,
+    village: 'Caddington',
+    category: 'Table Pieces',
+    title: "Swinging Gate",
+    itemUrl: 'https://www.lemaxcollection.com/villages/caddington/table-pieces/swinging-gate-84351',
+    imageUrl: '../../assets/images/houses/02-swinginggate.png',
+    retired: false,
+    sku: 84351,
+    yearReleased: 2018,
+    yearRetired: 0,
+    routeName: 'swinging-gate',
+  },
+};  
+
+export default SCENES_DATA;
