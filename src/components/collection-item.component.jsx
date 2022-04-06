8766bca6839ef1aeb8fc0102ee416dd4a6ab8bdf
Haylee <17357481+hailstormCodes@users.noreply.github.com>
2022-04-06 16:32:41 -0400

    commit things that have changed

--- src/components/collection-item.component.jsx
+++ src/components/collection-item.component.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import CustomButton from '../custom-button/custom-button.component';
-import { addItem } from '../../redux/cart/cart.actions';
-
 import './collection-item.styles.scss';
 
 const CollectionItem = ({ item, addItem }) => {
@@ -17,13 +14,6 @@ const CollectionItem = ({ item, addItem }) => {
           backgroundImage: `url(${imageUrl})`
         }}
       />
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-      </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
-        Add to cart
-      </CustomButton>
     </div>
   );
 };
